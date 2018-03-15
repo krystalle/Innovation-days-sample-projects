83742a1f657f4721828c8c9242f8ee74ad76e4a6
kle2 <krystal_le@intuit.com>
2018-03-15 16:52:15 -0700

    Use remote webdriver

--- sample-UI/src/test/java/com/test/ui/SampleWebTest.java
+++ sample-UI/src/test/java/com/test/ui/SampleWebTest.java
@@ -1,17 +1,21 @@
 package com.test.ui;
 
+import java.net.URL;
+
 import org.openqa.selenium.By;
 import org.openqa.selenium.Keys;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
 import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.chrome.ChromeOptions;
+import org.openqa.selenium.remote.RemoteWebDriver;
 import org.testng.Assert;
 import org.testng.annotations.Test;
 
 
 public class SampleWebTest {
-	
-	@Test
+
+	//@Test
 	public void googleTest() {
 		System.setProperty("webdriver.chrome.driver", "src/test/resources/drivers/chrome/linux/chromedriver");
 		WebDriver driver = new ChromeDriver();
@@ -20,14 +24,36 @@ public class SampleWebTest {
 		LogUtil.log("Getting the Google URL");
 		driver.get("https://www.google.com/");
 		LogUtil.log("Verifying the title");
-        Assert.assertEquals(driver.getTitle(), "Google");
-		
-        LogUtil.log("Searching for Intuit");
+		Assert.assertEquals(driver.getTitle(), "Google");
+
+		LogUtil.log("Searching for Intuit");
 		WebElement searchBox = driver.findElement(By.name("q"));
-     	searchBox.sendKeys("intuit");
-     	searchBox.sendKeys(Keys.ENTER);
-     	LogUtil.log("Verifying the title");
-     	Assert.assertEquals(driver.getTitle(), "intuit - Google Search");
+		searchBox.sendKeys("intuit");
+		searchBox.sendKeys(Keys.ENTER);
+		LogUtil.log("Verifying the title");
+		Assert.assertEquals(driver.getTitle(), "intuit - Google Search");
+
+		driver.quit();
+
+	}
+
+	@Test
+	public void googleTest2() throws Exception{
+
+		WebDriver driver = new RemoteWebDriver(new URL("http://aa319bb4726fd11e8883b025e65e4e41-397059983.us-west-2.elb.amazonaws.com:4444"), new ChromeOptions());
+
+		LogUtil.log("Launched the browser");
+		LogUtil.log("Getting the Google URL");
+		driver.get("https://www.google.com/");
+		LogUtil.log("Verifying the title");
+		Assert.assertEquals(driver.getTitle(), "Google");
+
+		LogUtil.log("Searching for Intuit");
+		WebElement searchBox = driver.findElement(By.name("q"));
+		searchBox.sendKeys("intuit");
+		searchBox.sendKeys(Keys.ENTER);
+		LogUtil.log("Verifying the title");
+		Assert.assertEquals(driver.getTitle(), "intuit - Google Search");
 
 		driver.quit();
 
